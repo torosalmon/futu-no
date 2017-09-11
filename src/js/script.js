2fceaa188ef136406ad3d4f7b6f9f3f7f25a3079
torosalmon <trs.torosalmon@gmail.com>
2017-09-11 22:48:42 +0900

    ud

--- src/js/script.js
+++ src/js/script.js
@@ -7,8 +7,12 @@
 !*/
 
 // =============================================================================
-// Passive Event Listeners - Feature Detection
+// Polyfill
 // =============================================================================
+
+// =======================
+// Passive Event Listeners
+// =======================
 
 var supportsPassive = false;
 try {
@@ -18,18 +22,13 @@ try {
     }
   });
   window.addEventListener('test', null, opts);
-}
-catch(e) {}
+} catch (e) {}
 
-// =============================================================================
-// requestAnimationFrame - Polyfill
-// =============================================================================
+// =====================
+// requestAnimationFrame
+// =====================
 
-window.requestAnimationFrame = window.requestAnimationFrame
-                            || window.mozRequestAnimationFrame
-                            || window.webkitRequestAnimationFrame
-                            || window.oRequestAnimationFrame
-                            || window.msRequestAnimationFrame;
+window.requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame;
 
 // =============================================================================
 // Mainstream
@@ -42,7 +41,8 @@ window.requestAnimationFrame = window.requestAnimationFrame
   // グローバル変数
   // ==============
 
-  const $body   = document.getElementsByTagName('body')[0];
+  const $meta_theme_color = document.querySelector('meta[name="theme-color"]');
+  const $body = document.getElementsByTagName('body')[0];
   const $header = document.getElementsByTagName('header')[0];
   const $drawer = document.getElementsByClassName('drawer')[0];
 
@@ -51,23 +51,18 @@ window.requestAnimationFrame = window.requestAnimationFrame
   // ============
 
   // <header> border-top-colorからアクセントカラーを取得
-  let color_accent = getComputedStyle($header);
-      color_accent = color_accent.borderTopColor;
-
+  const color_accent = getComputedStyle($header).borderTopColor;
   // <.drawer> background-colorからサブカラーを取得
-  let color_sub = getComputedStyle($drawer);
-      color_sub = color_sub.backgroundColor;
-
+  const color_sub = getComputedStyle($drawer).backgroundColor;
   // テーマカラーセット
-  let $meta_accent_color = document.querySelector('meta[name="theme-color"]');
-      $meta_accent_color.setAttribute('content', color_accent);
+  $meta_theme_color.setAttribute('content', color_accent);
 
   // ================
   // DOMContentLoaded
   // ================
 
   document.addEventListener('DOMContentLoaded', function(e) {
-    const menu         = new MENU();
+    const menu = new MENU();
     const html_support = new HTML_SUPPORT();
   }, false);
 
@@ -80,8 +75,8 @@ window.requestAnimationFrame = window.requestAnimationFrame
     constructor() {
       this.scroll_header();
       this.drawer();
-      this.share_overlay();
       this.search_form();
+      this.share_overlay();
     }
 
     // ===============================
@@ -89,10 +84,15 @@ window.requestAnimationFrame = window.requestAnimationFrame
     // ===============================
 
     scroll_header() {
-      const offset = 80; // 起動させるスクロール座標
+      // 起動させるスクロール座標
+      const offset = 80;
 
-      window.addEventListener('scroll', event_interval, supportsPassive ? { passive: true } : false);
-      document.addEventListener('touchmove', event_interval, supportsPassive ? { passive: true } : false);
+      window.addEventListener('scroll', event_interval, supportsPassive ? {
+        passive: true
+      } : false);
+      document.addEventListener('touchmove', event_interval, supportsPassive ? {
+        passive: true
+      } : false);
 
       // イベント発火調整
       function event_interval() {
@@ -101,14 +101,14 @@ window.requestAnimationFrame = window.requestAnimationFrame
 
       // 表示切り替え
       function toggle() {
-        if(window.pageYOffset > offset) {
+        if (window.pageYOffset > offset) {
           $body.classList.add('show-scroll-header');
-        }
-        else {
+        } else {
           $body.classList.remove('show-scroll-header');
         }
       }
       toggle();
+
     }
 
     // ========
@@ -116,27 +116,58 @@ window.requestAnimationFrame = window.requestAnimationFrame
     // ========
 
     drawer() {
-
-      // ============
-      // ドロワー開閉
-      // ============
-
       const $toggle = document.getElementsByClassName('js-drawer-toggle')[0];
       $toggle.addEventListener('click', function() {
-        if(!$body.classList.contains('show-drawer')) {
+
+        // 開く
+        if (!$body.classList.contains('show-drawer')) {
+          // ステータス変更
           $body.classList.remove('show-search-form');
           $body.classList.add('show-drawer');
-
-          $meta_accent_color.setAttribute('content', color_sub);
+          // テーマカラー変更
+          $meta_theme_color.setAttribute('content', color_sub);
           $header.style.borderTopColor = color_sub;
         }
-        else {
-          $body.classList.remove('show-drawer');
 
-          $meta_accent_color.setAttribute('content', color_accent);
+        // 閉じる
+        else {
+          // ステータス変更
+          $body.classList.remove('show-drawer');
+          // テーマカラー変更
+          $meta_theme_color.setAttribute('content', color_accent);
           $header.style.borderTopColor = color_accent;
         }
+
       }, false);
+    }
+
+    // ============
+    // 検索フォーム
+    // ============
+
+    search_form() {
+
+      // 開く
+      const $open = document.getElementsByClassName('js-search-form-open')[0];
+      $open.addEventListener('click', function() {
+        // ステータス変更
+        $body.classList.remove('show-drawer');
+        $body.classList.add('show-search-form');
+        // テーマカラー変更
+        $meta_theme_color.setAttribute('content', color_sub);
+        $header.style.borderTopColor = color_sub;
+      }, false);
+
+      // 閉じる
+      const $close = document.getElementsByClassName('js-search-form-close')[0];
+      $close.addEventListener('click', function() {
+        // ステータス変更
+        $body.classList.remove('show-search-form');
+        // テーマカラー変更
+        $meta_theme_color.setAttribute('content', color_accent);
+        $header.style.borderTopColor = color_accent;
+      }, false);
+
     }
 
     // ================
@@ -145,22 +176,20 @@ window.requestAnimationFrame = window.requestAnimationFrame
 
     share_overlay() {
 
-      // ================
-      // オーバーレイ開閉
-      // ================
-
+      // 開く
       const $open = document.getElementsByClassName('js-share-overlay-open')[0];
       $open.addEventListener('click', function() {
+        // ステータス変更
         $body.classList.add('show-share-overlay');
-
         // 背景スクロール開始
         background_scroll('start');
       }, false);
 
+      // 閉じる
       const $close = document.getElementsByClassName('js-share-overlay-close')[0];
       $close.addEventListener('click', function() {
+        // ステータス変更
         $body.classList.remove('show-share-overlay');
-
         // 背景スクロール停止
         background_scroll('stop');
       }, false);
@@ -170,54 +199,27 @@ window.requestAnimationFrame = window.requestAnimationFrame
       // ==================
 
       let timer;
+
       function background_scroll(arg_state) {
 
         // 開始
-        if(arg_state === 'start') {
+        if (arg_state === 'start') {
           timer = setInterval(function() {
-            if(window.pageYOffset === document.body.scrollHeight - window.innerHeight) {
+            if (window.pageYOffset === document.body.scrollHeight - window.innerHeight) {
               window.scrollTo(0, 0);
-            }
-            else {
+            } else {
               window.scrollBy(0, 1);
             }
           }, 30);
         }
 
         // 停止
-        else if(arg_state === 'stop') {
+        else if (arg_state === 'stop') {
           clearInterval(timer);
         }
+
       }
 
-    }
-
-    // ============
-    // 検索フォーム
-    // ============
-
-    search_form() {
-
-      // ============
-      // フォーム開閉
-      // ============
-
-      const $open = document.getElementsByClassName('js-search-form-open')[0];
-      $open.addEventListener('click', function() {
-        $body.classList.remove('show-drawer');
-        $body.classList.add('show-search-form');
-
-        $meta_accent_color.setAttribute('content', color_sub);
-        $header.style.borderTopColor = color_sub;
-      }, false);
-
-      const $close = document.getElementsByClassName('js-search-form-close')[0];
-      $close.addEventListener('click', function() {
-        $body.classList.remove('show-search-form');
-
-        $meta_accent_color.setAttribute('content', color_accent);
-        $header.style.borderTopColor = color_accent;
-      }, false);
     }
 
   }
@@ -230,10 +232,10 @@ window.requestAnimationFrame = window.requestAnimationFrame
 
     constructor() {
       this.smooth_scroll();
-      if(document.querySelector('pre code') != null) {
+      if (document.querySelector('pre code') != null) {
         this.source_highlight();
       }
-      if(document.getElementsByTagName('video')[0] != null) {
+      if (document.getElementsByTagName('video')[0] != null) {
         this.inline_playing_video();
       }
     }
@@ -248,20 +250,33 @@ window.requestAnimationFrame = window.requestAnimationFrame
 
     smooth_scroll() {
       const sweetScroll = new SweetScroll({
-        trigger: '.js-scroll',   // トリガーとなる要素をCSSセレクタで指定
-        header: 'header',        // 固定ヘッダをCSSセレクタで指定
-        duration: 900,           // アニメーション再生時間のミリ秒
-        delay: 0,                // アニメーション開始までの遅延ミリ秒
-        easing: 'easeOutExpo',   // イージングのタイプ（デフォルト：easeOutQuint）
-        offset: 0,               // スクロール位置のオフセット
-        verticalScroll: true,    // 垂直方向のスクロールを許可
-        horizontalScroll: false, // 水平方向のスクロールを許可 (デフォルトでは無効)
-        stopScroll: true,        // ホイール・タッチイベントが発生した時にスクロールを停止
+        // トリガーとなる要素をCSSセレクタで指定
+        trigger: '.js-scroll',
+        // 固定ヘッダをCSSセレクタで指定
+        header: 'header',
+        // アニメーション再生時間のミリ秒
+        duration: 900,
+        // アニメーション開始までの遅延ミリ秒
+        delay: 0,
+        // イージングのタイプ（デフォルト：easeOutQuint）
+        easing: 'easeOutExpo',
+        // スクロール位置のオフセット
+        offset: 0,
+        // 垂直方向のスクロールを許可
+        verticalScroll: true,
+        // 水平方向のスクロールを許可 (デフォルトでは無効)
+        horizontalScroll: false,
+        // ホイール・タッチイベントが発生した時にスクロールを停止
+        stopScroll: true,
 
         // Callbacks
-        beforeScroll: null,      // スクロールが始まる前 (return falseでキャンセル可)
-        afterScroll: null,       // スクロールが終わった時
-        cancelScroll: null       // スクロールがキャンセルされた時
+
+        // スクロールが始まる前 (return falseでキャンセル可)
+        beforeScroll: null,
+        // スクロールが終わった時
+        afterScroll: null,
+        // スクロールがキャンセルされた時
+        cancelScroll: null
       });
     }
 
@@ -283,7 +298,7 @@ window.requestAnimationFrame = window.requestAnimationFrame
 
     inline_playing_video() {
       const $video = document.getElementsByTagName('video');
-      for(let i = 0; i < $video.length; i ++) {
+      for (let i = 0; i < $video.length; i++) {
         $video[i].setAttribute('webkit-playsinline', 'webkit-playsinline');
       }
     }
